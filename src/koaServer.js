7773bec90e460c49b3a2ed05e4547df432c74b78
Simon Schlaetker <s.schlaetker@gmx.net>
2018-03-11 19:12:18 +0100

    msg

--- src/koaServer.js
+++ src/koaServer.js
@@ -1,9 +1,20 @@
 //192.168.178.84
 const Koa = require("koa");
+const Router = require("koa-router");
 const serve = require('koa-static');
+const shutdown = require('./shutdown');
 
 const app = new Koa();
+const router = new Router();
 
+router.get('/sleep', (ctx, next) => {
+
+    shutdown.sleep();
+    next();
+
+});
+
+app.use(router.routes());
 app.use(serve("../static/"));
 
 app.listen(5050);
